41bcb89c5b262b56092cc3d8390424c7f8e1ab62
okskygo <okskygo@gmail.com>
2017-12-20 11:19:44 +0800

    hide subject

--- app/src/main/java/com/rex/easytablayout/MainActivity.kt
+++ app/src/main/java/com/rex/easytablayout/MainActivity.kt
@@ -9,34 +9,30 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : RxAppCompatActivity() {
 
-  private val factory: TabFactory<MainTabSource> = object : TabFactory<MainTabSource>() {
-
-    init {
-      onTabSelectedSubject
-          .compose(bindUntilEvent(ActivityEvent.DESTROY))
-          .subscribe {
-            /*
-            when (it) {
-              is FirstTabSource -> TODO()
-              is SecondTabSource -> TODO()
-              is ThirdTabSource -> TODO()
-              is FourthTabSource -> TODO()
-            }
-            */
-            Toast.makeText(this@MainActivity, it.title, Toast.LENGTH_SHORT).show()
-          }
-    }
-
-    override val available = arrayListOf(FirstTabSource(),
-        SecondTabSource(),
-        ThirdTabSource(),
-        FourthTabSource())
-  }
+  private val availableTabs = arrayListOf(FirstTabSource(),
+      SecondTabSource(),
+      ThirdTabSource(),
+      FourthTabSource())
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-    tabLayout.setupWithViewPager(pager, supportFragmentManager, factory)
+    val tabFactory = TabFactory.Builder().create(availableTabs)
+    tabLayout.setupWithViewPager(pager, supportFragmentManager, tabFactory)
+
+    tabFactory.onTabSelected().compose(bindUntilEvent(ActivityEvent.DESTROY))
+        .subscribe {
+          /*
+          when (it) {
+            is FirstTabSource -> TODO()
+            is SecondTabSource -> TODO()
+            is ThirdTabSource -> TODO()
+            is FourthTabSource -> TODO()
+          }
+          */
+          Toast.makeText(this, it.title, Toast.LENGTH_SHORT).show()
+        }
+
   }
 }
 
--- app/src/main/java/com/rex/easytablayout/tab/EasyTabLayout.kt
+++ app/src/main/java/com/rex/easytablayout/tab/EasyTabLayout.kt
@@ -1,10 +1,16 @@
 package com.rex.easytablayout.tab
 
 import android.content.Context
+import android.os.Bundle
+import android.support.annotation.DrawableRes
+import android.support.annotation.StringRes
 import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.view.ViewPager
 import android.util.AttributeSet
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
 
 class EasyTabLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null)
   : TabLayout(context, attrs) {
@@ -12,24 +18,76 @@ class EasyTabLayout @JvmOverloads constructor(context: Context, attrs: Attribute
   fun <T : TabSource> setupWithViewPager(viewPager: ViewPager,
                                          fragmentManager: FragmentManager,
                                          factory: TabFactory<T>) {
-
     val tabPagerAdapter = TabPagerAdapter(fragmentManager, factory)
     viewPager.adapter = tabPagerAdapter
     super.setupWithViewPager(viewPager)
-    addOnTabSelectedListener(ViewPagerOnTabSelectedListener(viewPager, tabPagerAdapter))
+    addOnTabSelectedListener(EasyTabLayout.ViewPagerOnTabSelectedListener(viewPager,
+        tabPagerAdapter))
     tabPagerAdapter.initCustomTabs(this)
-
   }
 
-  class ViewPagerOnTabSelectedListener<T : TabSource>(viewPager: ViewPager,
-                                                      private val tabPagerAdapter: TabPagerAdapter<T>)
+  private class ViewPagerOnTabSelectedListener<T : TabSource>(viewPager: ViewPager,
+                                                              private val tabPagerAdapter: TabPagerAdapter<T>)
     : TabLayout.ViewPagerOnTabSelectedListener(viewPager) {
-
     override fun onTabSelected(tab: Tab?) {
       super.onTabSelected(tab)
       val position = tab?.position ?: return
-      tabPagerAdapter.factory.apply { onTabSelectedSubject.onNext(available[position]) }
+      val factory = tabPagerAdapter.factory as? TTabFactory ?: return
+      factory.onTabSelectedSubject.onNext(factory.available[position])
+    }
+  }
+}
+
+abstract class TabFactory<T : TabSource>(open val available: List<T>,
+                                         open val bundle: Bundle? = null) {
+
+  abstract fun onTabSelected(): Observable<T>
+
+  class Builder {
+
+    private var bundle: Bundle? = null
+
+    fun addBundle(bundle: Bundle) {
+      this.bundle = bundle
     }
 
+    fun <T : TabSource> create(available: List<T>): TabFactory<T> {
+      return TTabFactory(available, bundle)
+    }
+  }
+}
+
+private class TTabFactory<T : TabSource> constructor(override val available: List<T>,
+                                                     override val bundle: Bundle? = null) : TabFactory<T>(
+    available,
+    bundle) {
+
+  val onTabSelectedSubject: PublishSubject<T> = PublishSubject.create<T>()
+
+  override fun onTabSelected(): Observable<T> = onTabSelectedSubject
+
+}
+
+sealed class TabSource {
+
+  abstract class ImageTabSource : TabSource() {
+    @get:DrawableRes
+    abstract val icon: Int
+  }
+
+  abstract class TextTabSource : TabSource()
+
+  @get:StringRes
+  abstract val title: Int
+  abstract protected val fragmentClass: Class<out Fragment>
+
+  val fragmentInstance: Fragment? by lazy {
+    try {
+      fragmentClass.newInstance()
+    } catch (e: InstantiationException) {
+      null
+    } catch (e: IllegalAccessException) {
+      null
+    }
   }
 }
